3820ebe1d6113e45da9df24034c5510184520b0b
FilippoVicini <filippo.vicini2@gmail.com>
2025-02-19 12:19:20 +0000

    feat: initialize db connection

--- app/mkApp/src/main/java/com/lancaster/App.java
+++ app/mkApp/src/main/java/com/lancaster/App.java
@@ -1,13 +1,22 @@
-package com.example;
+package com.lancaster;
 
-/**
- * Hello world!
- *
- */
+import com.lancaster.util.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            System.out.println("Database connected successfully!");
+
+
+
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
     }
 }
